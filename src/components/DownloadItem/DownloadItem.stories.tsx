ca4f0698ac2bd6fe21038f174a3b715778632d18
SpaceMan28 <nicokazemi34@gmail.com>
2023-10-31 19:02:54 -0600

    refactor: refactor story

--- src/components/DownloadItem/DownloadItem.stories.tsx
+++ src/components/DownloadItem/DownloadItem.stories.tsx
@@ -1,5 +1,13 @@
 import { DownloadItem } from "./DownloadItem";
+import type { Meta, StoryObj } from '@storybook/react';
 
-export default {
+const meta: Meta<typeof DownloadItem> = {
     component: DownloadItem
 };
+export default meta;
+
+type Story = StoryObj<typeof DownloadItem>;
+
+export const Primary: Story = {
+    render: () => <DownloadItem />
+}
