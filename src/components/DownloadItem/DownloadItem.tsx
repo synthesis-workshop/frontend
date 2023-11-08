b6a2f838db0a8612ba5da9aed16bd9083c93d91a
SpaceMan28 <nicokazemi34@gmail.com>
2023-11-08 14:41:07 -0700

    feature: add base button component to DownloadItem component

--- src/components/DownloadItem/DownloadItem.tsx
+++ src/components/DownloadItem/DownloadItem.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Button } from '../button/button';
 
 interface DownloadItemProps {
     title: string,
@@ -15,7 +16,7 @@ export const DownloadItem: React.FC<DownloadItemProps> = ({title, lastUpdated}) 
                 <div className='mb-2 font-text text-2xl text-primary tracking-tight font-medium'>{title}</div>
                 <div className='font-text text-base opacity-80 text-primary tracking-tight font-normal'>Last Updated: {lastUpdated}</div>
             </div>
-            <button className='font-text text-lg'>Download</button>
+            <Button variant="primary" size='default'>Download ↓</Button>
         </div>
     )
 }
