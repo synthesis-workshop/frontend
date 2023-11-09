ba7f5e12750bfe64cb21ac408ac222fe6d0b75e9
SpaceMan28 <nicokazemi34@gmail.com>
2023-11-09 08:36:18 -0700

    refactor: add classname to downloaditem

--- src/components/DownloadItem/DownloadItem.tsx
+++ src/components/DownloadItem/DownloadItem.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button } from '../button/button';
+import { Button } from '../button/index';
 
 interface DownloadItemProps {
     title: string,
@@ -11,12 +11,12 @@ interface DownloadItemProps {
 
 export const DownloadItem: React.FC<DownloadItemProps> = ({title, lastUpdated}) => {
     return (
-        <div className='flex justify-between'>
+        <div className='flex justify-between items-center'>
             <div>
                 <div className='mb-2 font-text text-2xl text-primary tracking-tight font-medium'>{title}</div>
                 <div className='font-text text-base opacity-80 text-primary tracking-tight font-normal'>Last Updated: {lastUpdated}</div>
             </div>
-            <Button variant="primary" size='default'>Download ↓</Button>
+            <Button variant="primary" size='default' className='h-[52px]'>Download ↓</Button>
         </div>
     )
 }
