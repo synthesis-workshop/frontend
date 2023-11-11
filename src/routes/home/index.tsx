9685fab48cd340299d9271598a7564f690660ad0
andkirsten <and.kirsten@gmail.com>
2023-11-11 20:07:01 +0900

    feat: spacing corrected, still lacking colored blobs

--- src/routes/home/index.tsx
+++ src/routes/home/index.tsx
@@ -6,6 +6,6 @@ import { Hero } from "../../components/hero";
 
 export const Home = () => {
   return <div>
-    <Hero videoCount={"102+"} viewsCount={"295.2K+"} guestsCount={"97+"} subCount={"9.2K+"} />
+    <Hero videoCount={"140+"} viewsCount={"295.2K+"} guestsCount={"97+"} subCount={"9.20K+"} />
   </div>;
 };
