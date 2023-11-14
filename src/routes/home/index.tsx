853e668f9d9d8c504d3ab35eb99b72c301220712
andkirsten <and.kirsten@gmail.com>
2023-11-14 11:01:23 +0900

    fix: removed Hero content from the home page, used just for testing

--- src/routes/home/index.tsx
+++ src/routes/home/index.tsx
@@ -1,11 +1,8 @@
 // routes/home/index.tsx
 
-import { Hero } from "../../components/hero";
-
-
 
 export const Home = () => {
   return <div>
-    <Hero videoCount={"140+"} viewsCount={"295.2K+"} guestsCount={"97+"} subCount={"9.20K+"} />
+    Home page content goes here
   </div>;
 };
