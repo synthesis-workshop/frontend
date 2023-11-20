0643c3582eae73b3e5b2501e8e0b75a86e0c4389
Josh Horwitz <josh.horwitz@floqast.com>
2023-11-20 01:25:32 -0500

    fix: updated index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,4 +4,5 @@ export * from "./download-item";
 export * from "./loading";
 export * from "./poster-card";
 export * from "./problem-set-card";
+export * from "./publication-card";
 export * from "./team-member";
