49b08ecd1ddb9e31311ed5760a65d6090a2b21e9
Josh Horwitz <josh.horwitz@floqast.com>
2023-11-19 19:45:12 -0500

    fix: moved episode section to routes dir

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,7 @@
 export * from "./button";
 export * from "./card";
 export * from "./download-item";
+export * from "./episode-card";
 export * from "./poster-card";
 export * from "./problem-set-card";
 export * from "./team-member";
--- src/routes/home/index.tsx
+++ src/routes/home/index.tsx
@@ -1,43 +1,51 @@
 // routes/home/index.tsx
-import { EpisodeCard } from "../../components/episode-card";
-import EpisodesSection from "../../components/episodes-section/episodes-section";
+import { EpisodeCard } from "../../components";
+import { EpisodeCategory } from "../../types";
+import EpisodesSection from "./episodes-section";
+
 export const Home = () => {
   const cards: React.ReactElement[] = [
     <EpisodeCard
       title={"Trial-1"}
       description={"Trial"}
       episodeNumber={1}
-      episodeType={"Research Spotlight"}
+      category={EpisodeCategory.RESEARCH_SPOTLIGHT}
+      runtime="15:12"
     ></EpisodeCard>,
     <EpisodeCard
       title={"Trial-2"}
       description={"Trial"}
       episodeNumber={1}
-      episodeType={"Research Spotlight"}
+      category={EpisodeCategory.RESEARCH_SPOTLIGHT}
+      runtime="15:12"
     ></EpisodeCard>,
     <EpisodeCard
       title={"Trial-3"}
       description={"Trial"}
       episodeNumber={1}
-      episodeType={"Research Spotlight"}
+      category={EpisodeCategory.RESEARCH_SPOTLIGHT}
+      runtime="15:12"
     ></EpisodeCard>,
     <EpisodeCard
       title={"Trial-4"}
       description={"Trial"}
       episodeNumber={1}
-      episodeType={"Research Spotlight"}
+      category={EpisodeCategory.RESEARCH_SPOTLIGHT}
+      runtime="15:12"
     ></EpisodeCard>,
     <EpisodeCard
       title={"Trial-5"}
       description={"Trial"}
       episodeNumber={1}
-      episodeType={"Research Spotlight"}
+      category={EpisodeCategory.RESEARCH_SPOTLIGHT}
+      runtime="15:12"
     ></EpisodeCard>,
     <EpisodeCard
       title={"Trial-6"}
       description={"Trial"}
       episodeNumber={1}
-      episodeType={"Research Spotlight"}
+      category={EpisodeCategory.RESEARCH_SPOTLIGHT}
+      runtime="15:12"
     ></EpisodeCard>,
   ];
   return (
