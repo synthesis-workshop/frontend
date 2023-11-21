4756ddc2d3716cf206938dc9a72e601a7342b42a
Josh Horwitz <josh.horwitz@floqast.com>
2023-11-20 19:55:08 -0500

    fix: added index.ts file

--- src/components/video-player/video-player.tsx
+++ src/components/video-player/video-player.tsx
@@ -1,16 +1,30 @@
-import React from "react";
 import ReactPlayer from "react-player";
 
 interface Props {
-    className?: string;
-    url: string;
-    width: string;
-    height: string;
-    controls: boolean;
-    light: boolean;
+  className?: string;
+  url: string;
+  width: string;
+  height: string;
+  controls: boolean;
+  light: boolean;
 }
 
-export const VideoPlayer = ({className, url, width, height, controls, light}: Props) => {
-    return <ReactPlayer className={className} url={url} controls={controls} light={light} width={width} height={height}
+export const VideoPlayer = ({
+  className,
+  url,
+  width,
+  height,
+  controls = true,
+  light,
+}: Props) => {
+  return (
+    <ReactPlayer
+      className={className}
+      url={url}
+      controls={controls}
+      light={light}
+      width={width}
+      height={height}
     />
+  );
 };
