64c171cb6d3c5d6294690fbe024886d69c0422be
rezamohdev <mohammadireza29@gmail.com>
2023-11-21 20:50:54 -0500

    fix: add storybook entry

---
+++ src/components/searchbar/searchbar.stories.tsx
@@ -0,0 +1,15 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Searchbar } from "./searchbar";
+
+const meta: Meta<typeof Searchbar> = {
+  component: Searchbar,
+  title: "Components/Searchbar",
+};
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+export const Default: Story = {
+  args: {
+    placeholder: "A good placeholder",
+  },
+};
--- src/components/searchbar/searchbar.tsx
+++ src/components/searchbar/searchbar.tsx
@@ -2,22 +2,33 @@ import { Button } from "../button";
 import { Form } from "../reusable-form/Form";
 import { SingleLineTextInput } from "../reusable-form/SingleLineTextInput";
 
+const registering = () => {
+  console.log("login");
+};
 export const Searchbar = () => {
   return (
     <div>
       <Form formClassname="searchbar__form">
-        <SingleLineTextInput
-          placeholder="hello"
-          inputClassname="searchbar__input"
-          type="text"
-          ariaLabel="input"
-          inputId="5s4dsdkjflkjd"
-          name="name"
-          register="reg"
-          rules=""
-        />
+        <div className="flex gap-[20px]">
+          <SingleLineTextInput
+            placeholder="Search for any topic"
+            inputClassname="border-gray-400 border-2 rounded-lg py-4 px-5 h-[52px] w-full"
+            type="text"
+            ariaLabel="input"
+            inputId="5s4dsdkjflkjd"
+            name="searchInput"
+            register={registering()}
+            rules=""
+          />
+          <Button
+            className="px-8 py-2 h-[52px]"
+            size="default"
+            variant="primary"
+          >
+            Search
+          </Button>
+        </div>
       </Form>
-      <Button />
     </div>
   );
 };
