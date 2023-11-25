3f964c077cfc4524485e96484739afef2c9b561d
rezamohdev <mohammadireza29@gmail.com>
2023-11-24 19:42:58 -0500

    feat: add responsive to search-bar

--- src/components/searchbar/searchbar.tsx
+++ src/components/searchbar/searchbar.tsx
@@ -7,28 +7,22 @@ const registering = () => {
 };
 export const Searchbar = () => {
   return (
-    <div>
-      <Form formClassname="searchbar__form">
-        <div className="flex gap-[20px]">
-          <SingleLineTextInput
-            placeholder="Search for any topic"
-            inputClassname="border-gray-400 border-2 rounded-lg py-4 px-5 h-[52px] w-full"
-            type="text"
-            ariaLabel="input"
-            inputId="5s4dsdkjflkjd"
-            name="searchInput"
-            register={registering()}
-            rules=""
-          />
-          <Button
-            className="px-8 py-2 h-[52px]"
-            size="default"
-            variant="primary"
-          >
-            Search
-          </Button>
-        </div>
-      </Form>
-    </div>
+    <Form formClassname="searchbar__form">
+      <div className="flex flex-row gap-5 lg:flex-row  max-[375px]:flex-col sm:gap-3">
+        <SingleLineTextInput
+          placeholder="Search for any topic"
+          inputClassname="border-gray-400 border-2 rounded-lg py-4 px-5 h-[52px] w-full"
+          type="text"
+          ariaLabel="input"
+          inputId="5s4dsdkjflkjd"
+          name="searchInput"
+          register={registering()}
+          rules=""
+        />
+        <Button className="px-8 py-2 h-[52px]" size="default" variant="primary">
+          Search
+        </Button>
+      </div>
+    </Form>
   );
 };
