cd6f1ef397c57413a3c144ac0bd36afbdd205862
SpaceMan28 <nicokazemi34@gmail.com>
2023-11-29 09:28:29 -0700

    feat: create base download section

---
+++ src/routes/home/downloads.tsx
@@ -0,0 +1,8 @@
+export const Downloads: React.FC = () => {
+    return (
+        <section className="my-[100px] max-w-[1200px] mx-auto">
+            <h2 className="mb-9 font-title text-3xl text-primary">Downloads</h2>
+            <div>Placeholder for Download Items</div>
+        </section>
+    )
+}
--- src/routes/home/index.tsx
+++ src/routes/home/index.tsx
@@ -1,12 +1,13 @@
 // routes/home/index.tsx
 import { EpisodesSection } from "./episodes-section";
 import { Hero } from "./hero";
-
+import { Downloads } from "./downloads";
 export const Home = () => {
   return (
     <div className="h-screen flex flex-col items-center">
       <Hero />
       <EpisodesSection />
+      <Downloads />
     </div>
   );
 };
