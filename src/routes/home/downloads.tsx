7b7737e1d64534d3ae6c29f835488b68f615d654
SpaceMan28 <nicokazemi34@gmail.com>
2023-11-29 09:46:38 -0700

    feat: add styles to download item container

--- src/routes/home/downloads.tsx
+++ src/routes/home/downloads.tsx
@@ -1,8 +1,12 @@
+import { DownloadItem } from "../../components"
+
 export const Downloads: React.FC = () => {
     return (
-        <section className="my-[100px] max-w-[1200px] mx-auto">
+        <section className="my-[100px] w-[1200px]">
             <h2 className="mb-9 font-title text-3xl text-primary">Downloads</h2>
-            <div>Placeholder for Download Items</div>
+            <div className="p-10 bg-white">
+                <DownloadItem title="Here" lastUpdated="12-12-2022" fileDownload={{url: "string"}}  />
+            </div>
         </section>
     )
 }
