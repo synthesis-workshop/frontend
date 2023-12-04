0dda039cb9f01f317615a0454a44685b281f5420
Josh Horwitz <josh.horwitz@floqast.com>
2023-12-03 21:08:14 -0500

    fix: remove unused tailwind theme changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,6 @@ export default {
       colors: {
         primary: "#3A3C46",
         accent: "#F6C825",
-        greyBg: "#F0F0F0",
         green: "#4FA161",
         blue: "#575AA4",
         pink: "#94498C",
@@ -22,18 +21,6 @@ export default {
       fontFamily: {
         text: ["Inter", "Helvetica", "Arial", "sans"],
         title: ["Esteban", "cursive"],
-      },
-      spacing: {
-        "500px": "500px",
-        "200px": "200px",
-        "1200px": "1200px",
-        "276px": "276px",
-        "100px": "100px",
-        "57px": "57px",
-        "1400px": "1400px",
-      },
-      transitionDuration: {
-        2000: "2000ms",
       },
     },
   },
