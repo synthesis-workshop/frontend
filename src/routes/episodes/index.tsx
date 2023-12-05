24dd7d622ac5edb97f86874daa36e4dbac728cd0
andkirsten <and.kirsten@gmail.com>
2023-12-05 20:15:53 +0900

    feat: episodes page started

--- src/routes/episodes/index.tsx
+++ src/routes/episodes/index.tsx
@@ -1,3 +1,40 @@
+import React from "react";
+import { useQuery } from "@apollo/client";
+import { Loading, Button, ProblemSetCard } from "../../components";
+
+
+
 export const Episodes = () => {
-  return <div>This is the episodes page</div>;
+  const isMobile = window.innerWidth < 768;
+
+  const { loading, data } = useQuery(GET_PROBLEM_SETS,{
+    variables: {
+      orderBy: {
+        downloadCount: "desc",
+      },
+      take: isMobile ? 3 : 6,
+    },
+  });
+
+
+  return <div className="bg-grey mx-auto w-[343px] md:w-11/12 max-w-[1280px] flex flex-col items-center">
+  <div>
+    <h2 className="font-title text-[32px] mb-3">Episodes</h2>
+    <div className="flex flex-col items-center">
+      
+      {loading ? (
+          <Loading />
+      ) : (
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+          {data?.problemSets?.map((problemSet) => (
+            <ProblemSetCard key={problemSet.id} problemSet={problemSet} />
+          ))}
+          </div>
+      )}
+      
+      <Button className="mt-10" variant="primary">Show All Problem Sets</Button>
+      
+    </div>
+    </div>
+</div>;
 };
