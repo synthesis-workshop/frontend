627b899cf2adc4ce09fe5eb419a1152a9353ca6d
SpaceMan28 <nicokazemi34@gmail.com>
2023-12-09 12:06:43 -0700

    feature: add responsiveness to downloads view

--- src/routes/home/downloads.tsx
+++ src/routes/home/downloads.tsx
@@ -12,7 +12,7 @@ export const Downloads: React.FC = () => {
     })
 
     const downloads = data?.downloads?.map((download) => (
-        <DownloadItem key={download.id} title={download.title} lastUpdated={download.lastUpdated} fileDownload={download.fileDownload} className="border-b-[1px] border-[#3A3C4666] pb-6 mb-6 last-of-type:mb-0 last-of-type:border-b-0 last-of-type:pb-0" />
+        <DownloadItem key={download.id} title={download.title} lastUpdated={download.lastUpdated.slice(0, 10)} fileDownload={download.fileDownload} className="border-b-[1px] border-[#3A3C4666] pb-6 mb-6 last-of-type:mb-0 last-of-type:border-b-0 last-of-type:pb-0" />
     ))
 
     if (loading) {
@@ -22,11 +22,13 @@ export const Downloads: React.FC = () => {
 }
 
     return (
-        <section className="my-[100px] w-[1200px]">
+        <section className="my-[100px] mx-10 max-w-[1200px] min-w-[311px] w-full">
             <h2 className="mb-9 font-title text-3xl text-primary">Downloads</h2>
-            <ExpandableSection preview={downloads.slice(0, 3)} className="w-[1200px]">
-                {downloads.slice(3)}
-            </ExpandableSection>
+            <div className="w-full">
+                <ExpandableSection preview={downloads.slice(0, 3)} className="w-full">
+                    {downloads.slice(3)}
+                </ExpandableSection>
+            </div>
         </section>
     )
 }
