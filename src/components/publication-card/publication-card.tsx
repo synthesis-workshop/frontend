63c05b01ae7fe342e31a0bf0b6565f7721fd2d80
andkirsten <and.kirsten@gmail.com>
2023-12-26 16:37:03 +0900

    feat: updating publication card

--- src/components/publication-card/publication-card.tsx
+++ src/components/publication-card/publication-card.tsx
@@ -1,38 +1,33 @@
 import { Button } from "../button";
 import { Card } from "../card";
-import { PropsWithChildren } from "react";
-import type { Publication } from "../../__generated__/graphql";
+import { Publication } from "../../__generated__/graphql";
+import { DocumentRenderer } from "@keystone-6/document-renderer";
 
-interface Props {
-  publication: Publication;
-}
 
-export const PublicationCard = ({
-  publication
-}: PropsWithChildren<Props>) => {
-
+export const PublicationCard = ({ title, description, publishedDate, publisher, link, doi, author }: Publication) => {
+  
   return (
     <Card variant="light">
       <div className="flex justify-between">
         <div className="max-w-[680px]">
-          <h2 className="text-2xl font-medium mb-3">{publication.title}</h2>
-          <p className="text-primary mb-8 max-w-[699px]">{publication.description?.document}</p>
+          <h2 className="text-2xl font-medium mb-3">{title}</h2>
+          <DocumentRenderer document={description?.document} />
         </div>
-        <a href={publication.link || "#"} target="_blank" className="hidden lg:block">
+        <a href={link || "#"} target="_blank" className="hidden lg:block">
           <Button className="w-max">Read the Full Article ↗</Button>
         </a>
       </div>
       <div className="flex flex-wrap gap-x-3 gap-y-1 text-primary/80">
-        <p>By: {publication.author}</p>
+        <p>By: {author}</p>
         <span>|</span>
-        <p>Published: {publication.publishedDate}</p>
+        <p>Published: {publishedDate}</p>
         <span>|</span>
-        <p>In: {publication.publisher}</p>
+        <p>In: {publisher}</p>
         <span>|</span>
-        <p>DOI: {publication.doi}</p>
+        <p>DOI: {doi}</p>
       </div>
       <a
-        href={publication.link || "#"}
+        href={link || "#"}
         target="_blank"
         className="lg:hidden mt-5 m-auto sm:mr-0 sm:ml-auto"
       >
