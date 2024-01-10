1e024365e4bc8c2dc38d7a27b8f320497de77fca
Josh Horwitz <josh.horwitz@floqast.com>
2024-01-09 21:37:41 -0500

    fix: build errors and storybook

--- src/components/publication-card/publication-card.stories.tsx
+++ src/components/publication-card/publication-card.stories.tsx
@@ -12,18 +12,25 @@ type PubStory = StoryObj<typeof PublicationCard>;
 
 export const Default: PubStory = {
   args: {
-    publication: {
-      id: "1",
-      link: "https://www.youtube.com/",
-      title: "A community that educates itself",
-      description: {
-        document: "Matthew Horwitz, creator of the Synthesis Workshop video podcast, talks to Nature Chemistry about using podcasting as a driver of professional-level education in the chemistry community.",
-      },
-      author: "Katherine Geogheghan",
-      publishedDate: "14 November 2023",
-      publisher: "Nature Chemistry",
-      doi: "10.1038/s41557-023-01144-x",
+    id: "1",
+    link: "https://www.youtube.com/",
+    title: "A community that educates itself",
+    description: {
+      document: [
+        {
+          type: "paragraph",
+          children: [
+            {
+              text: "Matthew Horwitz, creator of the Synthesis Workshop video podcast, talks to Nature Chemistry about using podcasting as a driver of professional-level education in the chemistry community.",
+            },
+          ],
+        },
+      ],
     },
+    author: "Katherine Geogheghan",
+    publishedDate: "14 November 2023",
+    publisher: "Nature Chemistry",
+    doi: "10.1038/s41557-023-01144-x",
   },
 
   parameters: {
