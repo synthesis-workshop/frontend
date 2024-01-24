1a3a861bf73e2bef1450b64c850f2e8c10cc8946
Josh Horwitz <josh.horwitz@floqast.com>
2024-01-24 17:00:44 -0500

    fix: type issues and graphql queries

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,6 @@ export default {
         blue: "#575AA4",
         pink: "#94498C",
         yellow: "#B6A362",
-        brightYellow: "#F6C825",
         white: "#FFFFFF",
         red: "#ff0000",
         formBackground: "#4d4f58",
