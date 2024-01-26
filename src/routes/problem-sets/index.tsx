6ce828246f3b9dfba7072f61d9d551065fa645fa
andkirsten <and.kirsten@gmail.com>
2024-01-26 19:21:16 +0900

    feat: added skeleton loading and added alt text to home page images

--- src/routes/problem-sets/index.tsx
+++ src/routes/problem-sets/index.tsx
@@ -21,42 +21,36 @@ export const ProblemSetsPage: React.FC = () => {
 
   return (
     <div className="bg-black/[.06] w-full h-screen pt-28">
-      <div className="mx-auto w-[343px] md:w-11/12 px-4 xl:max-w-xlPageContent flex flex-col items-center">
-        <div className="w-full">
-          <h2 className="font-title text-[32px] text-primary mb-3">
-            Problem Sets
-          </h2>
-          <p className="font-text text-lg mb-8 text-primary/80">
-            Problem sets are small tasks in PDF format that will help you to
-            practice and understand concepts of our episodes
-          </p>
+      <div className="mx-auto w-[343px] md:w-11/12 max-w-[1280px] items-center mt-10">
+        <h2 className="font-title text-[32px] mb-3">Problem Sets</h2>
+        <p className="font-text text-lg mb-8 text-primary/80 md:max-w-xl">
+          Problem sets are small tasks in PDF format that will help you to
+          practice and understand concepts of our episodes
+        </p>
+        <div className="items-center">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+          {data?.problemSets?.map((problemSet) => (
+            <ProblemSetCard key={problemSet.id} problemSet={problemSet} />
+          )) || <><Skeleton height={316} /><Skeleton height={316} /><Skeleton height={316} /></>}
         </div>
-        {loading ? (
-          <Loading />
-        ) : (
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
-            {data?.problemSets?.map((problemSet) => (
-              <ProblemSetCard key={problemSet.id} problemSet={problemSet} />
-            ))}
-          </div>
-        )}
-        {data?.problemSetsCount &&
-          data.problemSets &&
-          data.problemSetsCount > data.problemSets.length && (
-            <Button
-              className="mt-10"
-              variant="primary"
-              onClick={() => {
-                fetchMore({
-                  variables: {
-                    skip: data?.problemSets?.length,
-                  },
-                });
-              }}
-            >
-              Load More
-            </Button>
-          )}
+          {data?.problemSetsCount &&
+            data.problemSets &&
+            data.problemSetsCount > data.problemSets.length && (
+              <Button
+                className="mt-10"
+                variant="primary"
+                onClick={() => {
+                  fetchMore({
+                    variables: {
+                      skip: data?.problemSets?.length,
+                    },
+                  });
+                }}
+              >
+                Load More
+              </Button>
+            )}
+        </div>
       </div>
     </div>
   );
