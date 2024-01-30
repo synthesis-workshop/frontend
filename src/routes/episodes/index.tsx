09df9c24533be99cc03703baa1273d42859eb7e8
andkirsten <and.kirsten@gmail.com>
2024-01-30 09:02:36 +0900

    feat: episodes page

--- src/routes/episodes/index.tsx
+++ src/routes/episodes/index.tsx
@@ -6,10 +6,6 @@ import { OrderDirection } from "../../__generated__/graphql";
 import { Button, EpisodeCard } from "../../components";
 import Menu from "../../components/drop-down-menu/drop-down-menu";
 import { GET_EPISODES } from "../../graphql";
-import Skeleton from "react-loading-skeleton";
-import "react-loading-skeleton/dist/skeleton.css";
-import { SearchBar } from "../../components/form/search-bar";
-import { useForm } from "react-hook-form";
 
 const CategoryList = [
   {
@@ -70,16 +66,6 @@ export const Episodes = () => {
     },
   });
 
-  const [showContent, setShowContent] = React.useState(false);
-
-  React.useEffect(() => {
-    const timeoutId = setTimeout(() => {
-      setShowContent(true);
-    }, 1500);
-
-    return () => clearTimeout(timeoutId);
-  }, []);
-
   return (
     <div className="bg-grey flex flex-col max-w-[1280px]">
       <div
@@ -100,43 +86,41 @@ export const Episodes = () => {
           <Menu title={"Sort"} list={SortList} changeMenu={changeSort} />
         </div>
       </div>
-      <div className="pb-8 hidden md:visible">
+      <div>
         <p>Search Bar</p>
       </div>
-      <div
-        className={`grid lg:grid-cols-3 lg:gap-5 auto-rows-[360px] gap-5 md:grid-cols-2 md:gap-3 sm:grid-cols-1`}
-      >
-        {showContent ? (
-          data?.episodes?.map((episode) => (
-            <EpisodeCard key={episode.id} {...episode} />
-          ))
-        ) : (
-          <>
-            <Skeleton height={319} />
-            <Skeleton height={319} />
-            <Skeleton height={319} />
-          </>
+      {loading ? (
+        <div className="mt-12">
+          <Loading />
+        </div>
+      ) : (
+        <>
+          <div
+            className={`grid lg:grid-cols-3 lg:gap-5 auto-rows-[360px] gap-5 md:grid-cols-2 md:gap-3 sm:grid-cols-1`}
+          >
+            {data?.episodes?.map((episode) => (
+              <EpisodeCard key={episode.id} {...episode} />
+            ))}
+          </div>
+        </>
+      )}
+      {data?.episodesCount &&
+        data.episodes &&
+        data.episodesCount > data.episodes.length && (
+          <Button
+            className="mt-10"
+            variant="primary"
+            onClick={() => {
+              fetchMore({
+                variables: {
+                  skip: data?.episodes?.length,
+                },
+              });
+            }}
+          >
+            Load More
+          </Button>
         )}
-      </div>
-      <div className="flex justify-center">
-        {data?.episodesCount &&
-          data.episodes &&
-          data.episodesCount > data.episodes.length && (
-            <Button
-              className="my-10"
-              variant="primary"
-              onClick={() => {
-                fetchMore({
-                  variables: {
-                    skip: data?.episodes?.length,
-                  },
-                });
-              }}
-            >
-              Load More
-            </Button>
-          )}
-      </div>
     </div>
   );
 };
