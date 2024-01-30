5b9c1254aee1563a6cbb276c3ae312a8cb510110
andkirsten <and.kirsten@gmail.com>
2024-01-30 09:39:36 +0900

    feat: episodes page finished except for search bar

--- src/routes/episodes/index.tsx
+++ src/routes/episodes/index.tsx
@@ -1,6 +1,5 @@
 
 import { useQuery } from "@apollo/client";
-import cx from "classnames";
 import { useState } from "react";
 import { useMediaQuery } from "react-responsive";
 import { OrderDirection } from "../../__generated__/graphql";
@@ -66,49 +65,47 @@ export const Episodes = () => {
     },
   });
 
-  return <div className="bg-grey w-screen px-4">
-  <div
-    className={cx(
-      "w-full justify-between items-center pb-8 gap-5",
-      "md:flex-row md:items-start md:gap-0 ",
-    )}
-  >
-    <h2 className="font-title text-primary text-3xl pt-32 pb-5">
-      Explore our episodes
-    </h2>
-    <div className="font-text flex md:flex-row gap-4 sm:flex-col">
+  return <div className="bg-grey px-4">
+    
+    <div className="flex md:flex-row gap-4 sm:flex-col pt-32 pb-8 justify-between">
+      <h2 className="font-title text-primary text-3xl">
+        Explore our episodes
+      </h2>
+      <div className="flex sm:flex-col md:flex-row gap-4">
       <Menu
         title={"Category"}
         list={CategoryList}
         changeMenu={changeCategory}
       />
       <Menu title={"Sort"} list={SortList} changeMenu={changeSort} />
+      </div>
+    </div>
+    <div className="pb-8 hidden md:visible">
+      <p>Search Bar</p>
+    </div>
+    <div
+      className={`grid lg:grid-cols-3 lg:gap-5 auto-rows-[360px] gap-5 md:grid-cols-2 md:gap-3 sm:grid-cols-1`}
+    >
+      {data?.episodes?.map((episode) => (
+        <EpisodeCard key={episode.id} {...episode} />
+      )) || <><Skeleton height={319} /><Skeleton height={319} /><Skeleton height={319} /></>}
+    </div>
+    <div className="flex justify-center">
+    {data?.episodesCount &&
+      data.episodes &&
+      data.episodesCount > data.episodes.length && (
+        <Button
+          className="my-10"
+          variant="primary" 
+          onClick={() => {
+            fetchMore({
+              variables: {
+                skip: data?.episodes?.length,
+              },
+            });
+      }}>Load More</Button>
+    )}
     </div>
   </div>
-  <div className="hidden md:visible">
-    <p>Search Bar</p>
-  </div>
-  <div
-    className={`grid lg:grid-cols-3 lg:gap-5 auto-rows-[360px] gap-5 md:grid-cols-2 md:gap-3 sm:grid-cols-1`}
-  >
-    {data?.episodes?.map((episode) => (
-      <EpisodeCard key={episode.id} {...episode} />
-    )) || <><Skeleton height={319} /><Skeleton height={319} /><Skeleton height={319} /></>}
-  </div>
-  {data?.episodesCount &&
-    data.episodes &&
-    data.episodesCount > data.episodes.length && (
-      <Button
-        className="mt-10"
-        variant="primary" 
-        onClick={() => {
-          fetchMore({
-            variables: {
-              skip: data?.episodes?.length,
-            },
-          });
-    }}>Load More</Button>
-  )}
-</div>
 }
 
