9659696e9efcbc44a1236ba84efe25b5d47cd27e
SpaceMan28 <nicokazemi34@gmail.com>
2024-01-30 17:13:49 -0700

    feature/courses-page-view

--- src/routes/courses/index.tsx
+++ src/routes/courses/index.tsx
@@ -12,39 +12,5 @@ export const Courses = () => {
     },
   });
 
-  return (
-    <div className="pt-28 bg-black/[.06]">
-      <div className="mx-auto flex flex-col items-center px-4 xl:max-w-xlPageContent">
-        <h1 className="font-title text-[32px] text-primary mb-3 w-full">
-          Courses on Organic Chemistry
-        </h1>
-        <div className="rounded-md border-2 border-black p-7 mb-8 w-full">
-          <h2 className="font-title text-card-title">
-            Contribute to our next course!
-          </h2>
-          <p className="font-text pt-3 max-w-[659px]">
-            If you would like to contribute to our next course, feel free to
-            reach out via email at{" "}
-            <a href="mailto:synthesisworkshopvideos@gmail.com">
-              synthesisworkshopvideos@gmail.com
-            </a>{" "}
-            to discuss the possibilities.
-          </p>
-        </div>
-        {loading ? (
-          <div className="mt-12">
-            <Loading />
-          </div>
-        ) : (
-          <>
-            <div className="mb-16 flex flex-col gap-6 w-full">
-              {data?.courses?.map((course) => (
-                <CourseCard key={course.id} {...course} />
-              ))}
-            </div>
-          </>
-        )}
-      </div>
-    </div>
-  );
+  return <div className="flex flex-col items-center">This is Courses</div>;
 };
