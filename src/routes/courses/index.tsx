c4a020f9fd2c0bed7d66aa539c31fe0d6ab8a3de
Josh Horwitz <josh.horwitz@floqast.com>
2024-02-01 19:43:11 -0500

    fix: some standardization on headers

--- src/routes/courses/index.tsx
+++ src/routes/courses/index.tsx
@@ -13,36 +13,38 @@ export const Courses = () => {
   });
 
   return (
-    <div className="pt-[152px] bg-black/[.06] flex flex-col items-center">
-      <h1 className="text-section-title md:text-[50px] text-primary font-title mb-10 max-w-smPageContent lg:max-w-lgPageContent md:max-w-mdPageContent xl:max-w-xlPageContent">
-        Courses on Organic Chemistry
-      </h1>
-      <div className=" border rounded-md border-2 border-black p-7 w-full mb-12 max-w-smPageContent lg:max-w-lgPageContent md:max-w-mdPageContent xl:max-w-xlPageContent">
-        <h3 className="font-title text-card-title">
-          Contribute to our next course!
-        </h3>
-        <p className="font-text pt-3 max-w-[659px]">
-          If you would like to contribute to our next course, feel free to reach
-          out via email at{" "}
-          <a href="mailto:synthesisworkshopvideos@gmail.com">
-            synthesisworkshopvideos@gmail.com
-          </a>{" "}
-          to discuss the possibilities.
-        </p>
-      </div>
-      {loading ? (
-        <div className="mt-12">
-          <Loading />
+    <div className="pt-28 bg-black/[.06]">
+      <div className="mx-auto flex flex-col items-center px-4 xl:max-w-xlPageContent">
+        <h1 className="font-title text-[32px] text-primary mb-3 w-full">
+          Courses on Organic Chemistry
+        </h1>
+        <div className="rounded-md border-2 border-black p-7 mb-8 w-full">
+          <h2 className="font-title text-card-title">
+            Contribute to our next course!
+          </h2>
+          <p className="font-text pt-3 max-w-[659px]">
+            If you would like to contribute to our next course, feel free to
+            reach out via email at{" "}
+            <a href="mailto:synthesisworkshopvideos@gmail.com">
+              synthesisworkshopvideos@gmail.com
+            </a>{" "}
+            to discuss the possibilities.
+          </p>
         </div>
-      ) : (
-        <>
-          <div className="mb-16 flex flex-col gap-6 w-full max-w-smPageContent lg:max-w-lgPageContent md:max-w-mdPageContent xl:max-w-xlPageContent">
-            {data?.courses?.map((course) => (
-              <CourseCard key={course.id} {...course} />
-            ))}
+        {loading ? (
+          <div className="mt-12">
+            <Loading />
           </div>
-        </>
-      )}
+        ) : (
+          <>
+            <div className="mb-16 flex flex-col gap-6 w-full">
+              {data?.courses?.map((course) => (
+                <CourseCard key={course.id} {...course} />
+              ))}
+            </div>
+          </>
+        )}
+      </div>
     </div>
   );
 };
