ef0212158e2d112d5507fecf1ae269f74c416aae
Ian <ifd730@gmail.com>
2024-02-28 06:24:32 -0800

    style: another font fix

--- src/components/drop-down-menu/drop-down-menu.tsx
+++ src/components/drop-down-menu/drop-down-menu.tsx
@@ -24,7 +24,7 @@ export const Menu = ({ list, changeMenu, title }: PropsWithChildren<Props>) => {
       {({ open }) => (
         <>
           <div className="relative">
-            <Listbox.Button className="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-[26px] text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 sm:text-sm sm:leading-6">
+            <Listbox.Button className="relative font-text w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-[26px] text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 sm:text-sm sm:leading-6">
               <span className="flex items-center">
                 <span className="block truncate">
                   {title}: {selected.length > 1 ? "..." : selected[0].name}
